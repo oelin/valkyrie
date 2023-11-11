45cf4a289952d3b9abf21a6bcabd310529d92d0e
Kale Champagnie <42823429+oelin@users.noreply.github.com>
2023-11-11 14:55:30 +0000

    Update __init__.py

--- valkyrie/__init__.py
+++ valkyrie/__init__.py
@@ -1,2 +1,20 @@
-from valkyrie.validated import Validated, Validator
-from valkyrie.scoped import Scoped, Scope
+"""Valkyrie"""
+
+from dataclasses import dataclass
+from functools import wraps
+from typing import Any, Type
+
+
+def validate(type: Type) -> Type:
+    """Creates a validated type."""
+
+    type = dataclass(frozen=True)(type)  # Immutability.
+    
+    @wraps(type)
+    def validate(*args, **kwargs) -> Any:
+        
+        instance = type(*args, **kwargs)
+        instance.validate()
+
+        return instance
+    return validate
