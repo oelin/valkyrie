536dba0177c0146435043d53a83156a2d85017ee
oelin <42823429+oelin@users.noreply.github.com>
2023-12-31 10:11:44 +0000

    Update __init__.py

--- valory/__init__.py
+++ valory/__init__.py
@@ -1,20 +1,43 @@
 """Valory."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass as dc
 from functools import wraps
 from typing import Any, Type
 
 
-def validate(type: Type) -> Type:
-    """Returns a validated type."""
+def dataclass(cls) -> dc:
+    """Create a validated dataclass.
 
-    type = dataclass(frozen=True)(type)  # Immutability.
+    Parameters
+    ----------
+
+    cls - The unvalidated class.
+
+    Returns
+    -------
+
+    dataclass - The validated dataclass.
+
+    Example
+    -------
+
+    >>> @dataclass
+    ... class User:
+    ...     username: str
+    ...     password: str
+    ...   
+    ...     def validate(self) -> None:
+    ...         assert isinstance(self.username, str)
+    ...         assert isinstance(self.password, str)
+    """
+
+    cls = dc(frozen=True)(cls)  # Immutability.
     
-    @wraps(type)
-    def validate(*args, **kwargs) -> Any:
+    @wraps(cls)
+    def dataclass(*args, **kwargs) -> cls:
         
-        instance = type(*args, **kwargs)
+        instance = cls(*args, **kwargs)
         instance.validate()
 
         return instance
-    return validate
+    return dataclass
